2219a2a11c47a488b1bdfbcc56dc22f5aff9f077
b4test <b4oris@hotmail.com>
2011-05-20 22:05:37 -0400

    Finished exercise 1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-		:content => "Ruby on Rails Tutorial Sample App | Home")
+                :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -39,6 +39,19 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                 :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do 
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                :content => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
